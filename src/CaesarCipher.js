89e1b5323919dfa10c5674c0970d01422768ee32
prschulz <prschulz88@gmail.com>
2014-12-02 10:03:20 -0800

    adding encrypt and decrypt functions. Tests passing.

---
+++ src/CaesarCipher.js
@@ -0,0 +1,34 @@
+var cipher = {
+  encrypt: function (message, key) {
+    message = message.split(' ').join('').toLowerCase(); //remove spaces and make lowercase
+    messageArr = message.split(''); //make array
+    var encryptedMessage = [];  //empty array for encrypted message
+
+    messageArr.forEach(function(c){
+      encryptedMessage.push(nextChar(c, key)); //push each letter into array after calling nextChar
+    });
+
+    return encryptedMessage.join('');  //join and return encrypted message
+  },
+
+  decrypt: function (message, key) {
+    message = message.split(' ').join('').toLowerCase(); //remove spaces and make lowercase
+    messageArr = message.split(''); //make array
+    var decryptedMessage = []; //empty array for decrypted message
+
+    messageArr.forEach(function(c){
+      decryptedMessage.push(lastChar(c, key)); //push each letter into array after calling lastChar
+    });
+
+    return decryptedMessage.join('');  //join and return decrypted message
+  },
+
+};
+
+function nextChar(c, key) {
+    return String.fromCharCode(c.charCodeAt(0) + key);
+}
+
+function lastChar(c, key) {
+  return String.fromCharCode(c.charCodeAt(0)-key);
+}
